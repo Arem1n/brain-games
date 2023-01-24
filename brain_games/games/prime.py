f949937cdfc50e8894667d38d311e06d8f1a34ff
Arem1n <aremgart@gmail.com>
2023-01-24 21:43:36 +0500

    feat: add game brain_prime

---
+++ brain_games/games/prime.py
@@ -0,0 +1,18 @@
+from random import randint
+
+
+TASK = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def is_prime(num):
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+
+def get_question_answer():
+    rnd_num = randint(1, 100)
+    correct_answer = "yes" if is_prime(rnd_num) else "no"
+    question = f"Question: {rnd_num}"
+    return question, correct_answer
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import brain_games.games.prime
+from brain_games.engine import run_game
+
+
+def main():
+    run_game(brain_games.games.prime)
+
+
+if __name__ == '__main__':
+    main()
